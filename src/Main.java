58ef2232319418858a8b5393887b6e6c59d9f42d
vaicekauskis <spookylt2@gmail.com>
2023-03-30 07:18:43 +0300

    Augalai + Delete + Edirt

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Plant> plants = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
         Plant aa = new Plant();
-        aa.setTitle("Rožė. ");
-        aa.setTitleLatin("Rosa. ");
+        aa.setTitle("Rožė");
+        aa.setTitleLatin("Rosa");
         aa.setAnnualPerennial(false);
-        aa.setPlantContinent("Kinija. ");
+        aa.setPlantContinent("Kinija");
         aa.setHeightAdultPlantInMeters(7);
         aa.setEdibleInedible(true);
-
 
         Plant aa2 = new Plant();
         aa2.setTitle("Triskiautė žibuoklė. ");
@@ -25,28 +24,41 @@ public class Main {
         Plant aa3 = new Plant("Saulėgrąža", "Helianthus", true, "Europa",
                 3, true);
 
-        plants.add(aa);
-        plants.add(aa2);
-        plants.add(aa3);
+        Plant.plants.add(aa);
+        Plant.plants.add(aa2);
+        Plant.plants.add(aa3);
 
-        for (int i = 0; i < plants.size(); i++) {
-            System.out.println(plants.get(i));
+        while (true) {
+            System.out.println("1. Pamatyti augalų sąrašą");
+            System.out.println("2. Pridėti augalą");
+            System.out.println("3. Redaguoti augalą");
+            System.out.println("4. Ištrinti augalą");
+            System.out.println("5. Išeiti iš programos");
+            int input = sc.nextInt();
+            sc.nextLine();
+            switch (input) {
+                case 1:
+                    Plant.printPlants();
 
+                    break;
+                case 2:
+                    Plant.addPlants(sc);
+                    break;
+                case 3:
+                    Plant.editPlant();
+
+                    break;
+                case 4:
+                    Plant.deletePlant();
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Pasirinkite validižią opciją");
+                    break;
+            }
         }
-
-        while (true){
-
-        }
-
-
-
-
-
-
-
-
-
-
 
 
     }
--- src/Plant.java
+++ src/Plant.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Plant {
-    private int id ;
+    public static ArrayList<Plant> plants = new ArrayList<>();
+    public static Scanner sc = new Scanner(System.in);
+    private static int idCount = 0;  //Statinis laukas
+
+    private final int id;
     private String title;
     private String titleLatin;
     private boolean annualPerennial;
@@ -10,11 +15,17 @@ public class Plant {
     private boolean edibleInedible;
 
     public Plant() {
+        this.id = ++idCount;
     }
 
+    public Plant(int id) {
+        this.id = id;
+    }
+
+
     public Plant(String title, String titleLatin, boolean annualPerennial, String plantcontinent,
-                 int heightAdultPlantInMeters,boolean edibleInedible) {
-        this.id = ++id;
+                 int heightAdultPlantInMeters, boolean edibleInedible) {
+        this.id = ++idCount;
         this.title = title;
         this.titleLatin = titleLatin;
         this.annualPerennial = annualPerennial;
@@ -24,80 +35,150 @@ public class Plant {
 
     }
 
-
-
+    public int getId() {
+        return this.id;
+    }
 
     public String getTitle() {
         return title;
     }
-    public void  setTitle(String title){
+
+    public void setTitle(String title) {
         this.title = title;
 
     }
-    public String getTitleLatin(){
+
+    public String getTitleLatin() {
         return titleLatin;
     }
-    public void setTitleLatin(String titleLatin){
+
+    public void setTitleLatin(String titleLatin) {
         this.titleLatin = titleLatin;
 
     }
-    public boolean  getAnnualPerennial(){
+
+    public boolean getAnnualPerennial() {
         return annualPerennial;
     }
-    public void setAnnualPerennial(boolean annualPerennial){
+
+    public void setAnnualPerennial(boolean annualPerennial) {
         this.annualPerennial = annualPerennial;
 
     }
-    public String getPlantContinent(){
+
+    public String getPlantContinent() {
         return plantContinent;
     }
-    public void setPlantContinent(String plantContinent){
-        this.plantContinent =plantContinent ;
+
+    public void setPlantContinent(String plantContinent) {
+        this.plantContinent = plantContinent;
 
     }
-    public double getHeightAdultPlantInMeters(){
+
+    public double getHeightAdultPlantInMeters() {
         return heightAdultPlantInMeters;
     }
-    public void  setHeightAdultPlantInMeters(double heightAdultPlantInMeters){
+
+    public void setHeightAdultPlantInMeters(double heightAdultPlantInMeters) {
         this.heightAdultPlantInMeters = heightAdultPlantInMeters;
 
     }
-    public boolean getedibleInedible(){
+
+    public boolean getedibleInedible() {
         return edibleInedible;
     }
 
-    public void setEdibleInedible(boolean edibleInedible){
+    public void setEdibleInedible(boolean edibleInedible) {
         this.edibleInedible = edibleInedible;
 
     }
+
     @Override
     public String toString() {
-        return  " Augalo pavadinimas: " + title +  " Pavadinimas Lotyniškai  :" + titleLatin + " Vienmetis :" +
-                (annualPerennial? " Taip " : " Ne ") + " Labiausiai paplitęs :" + plantContinent + " Suaugusio augalo aukštis " +
+        return id + ". Augalo pavadinimas: " + title + ". Pavadinimas Lotyniškai : " + titleLatin + ". Vienmetis :" +
+                (annualPerennial ? " Taip " : " Ne ") + " Labiausiai paplitęs :" + plantContinent + " Suaugusio augalo aukštis " +
                 (heightAdultPlantInMeters == (int) heightAdultPlantInMeters ? ((int) heightAdultPlantInMeters + "") :
-                        heightAdultPlantInMeters )+" Metrais " + " Ar valgomas :" + (edibleInedible? " Taip " : " Ne " );
+                        heightAdultPlantInMeters) + " Metrais " + " Ar valgomas :" + (edibleInedible ? " Taip " : " Ne ");
     }
 
 
-
-    public static void info (){
-        System.out.println("1. Pamatyti augalų sąrašą");
-        System.out.println("2. Pridėti augalą");
-        System.out.println("3. Redaguoti augalą");
-        System.out.println("4. Ištrinti augalą");
-        System.out.println("5. Išeiti iš programos");
+    public static void printPlants() {
+        for (int i = 0; i < plants.size(); i++) {
+            System.out.println(plants.get(i));
+        }
     }
 
+    public static void addPlants(Scanner sc) {
+        Plant plants = new Plant();
+        System.out.println("iveskite Augalo pavadinimą");
+        plants.setTitle(sc.nextLine());
+        System.out.println("iveskite Augalo pavadinimą Lotyniškai");
+        plants.setTitleLatin(sc.nextLine());
+        System.out.println("iveskite ar augalas vienmetis ");
+        plants.setAnnualPerennial(sc.nextBoolean());
+        sc.nextLine();
+        System.out.println("iveskite Augalo kontinentą");
+        plants.setPlantContinent(sc.nextLine());
+        System.out.println("iveskite suaugusio augalo aukštį");
+        plants.setHeightAdultPlantInMeters(sc.nextInt());
+        sc.nextLine();                                                 // nextLine() kad neprasoktu ir eitu užpildytį stulpelį
+        System.out.println("iveskite ar augalas valgomas ");
+        plants.setEdibleInedible(sc.nextBoolean());
+        Plant.plants.add(plants);
+    }
 
+    public static void editPlant() {
+        System.out.println("iveskite augalą Id kurį norite redaguoti");
+        int id = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < plants.size(); i++) {
+            if (plants.get(i).id == id) {
+                Plant aa4 = new Plant(id);
+                System.out.println("iveskite Augalo pavadinimą");
+                aa4.setTitle(sc.nextLine());
+                System.out.println("iveskite Augalo pavadinimą Lotyniškai");
+                aa4.setTitleLatin(sc.nextLine());
+                System.out.println("iveskite ar augalas vienmetis ");
+                aa4.setAnnualPerennial(sc.nextBoolean());
+                sc.nextLine();
+                System.out.println("iveskite Augalo kontinentą");
+                aa4.setPlantContinent(sc.nextLine());
+                System.out.println("iveskite suaugusio augalo aukštį");
+                aa4.setHeightAdultPlantInMeters(sc.nextInt());
+                sc.nextLine();
+                System.out.println("iveskite ar augalas valgomas ");
+                aa4.setEdibleInedible(sc.nextBoolean());
+                plants.set(i, aa4);
+                break;
+            }
+        }
+    }
 
+    public static void deletePlant() {
+        System.out.println("iveskite augalą  Is kurį norite redaguoti");
+        int id = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < plants.size(); i++) {
+            if (plants.get(i).id == id) {
+                plants.remove(i);
+                break;
 
-
-
-
-
-
-
-
-
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
